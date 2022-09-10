1582f3db91db8ccab4f3d935d7d7166b0307dc89
Anthony <onestlyof@gmail.com>
2022-09-10 20:40:18 +0300

    Creating main components

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+
+function App() {
+	return (
+		<div class='wrapper'>
+			<Header />
+			<div class='content'>
+				<div class='container'>
+					<div class='content__top'>
+						<Categories />
+						<Sort />
+					</div>
+					<h2 class='content__title'>Все пиццы</h2>
+					<div class='content__items'>
+						<PizzaBlock title="Мексиканская" price={450} />
+						<PizzaBlock title="Маргарита" price={350} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
